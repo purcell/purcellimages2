bab92178f459838b7c0e43c336338f07a10b8e34
Steve Purcell <steve@sanityinc.com>
2025-09-11 16:22:05 +0100

    Fix minor modes in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-((ocaml-ts-mode . ((ocp-indent-on-save-mode . t)))
- (nix-ts-mode . ((nixfmt-on-save-mode . t))))
+((ocaml-ts-mode . ((mode . ocp-indent-on-save)))
+ (nix-ts-mode . ((mode . nixfmt-on-save))))
